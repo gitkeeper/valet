89a27440987aea2734e80c498c308ce4e537734f
Alexander Baumann <alexander.baumann@arclight.ch>
2012-07-03 12:51:45 +0200

    Namespaced VERSION to Valet::VERSION to avoid conflicts (Rubinius, JRuby)

--- spec/valet/version_spec.rb
+++ spec/valet/version_spec.rb
@@ -1,19 +1,19 @@
 require 'spec_helper'
 
-describe VERSION do
+describe Valet::VERSION do
   it "has a major version number" do
-    VERSION::MAJOR.should be_an(Integer)
+    Valet::VERSION::MAJOR.should be_an(Integer)
   end
 
   it "has a minor version number" do
-    VERSION::MINOR.should be_an(Integer)
+    Valet::VERSION::MINOR.should be_an(Integer)
   end
 
   it "has a build version number" do
-    VERSION::BUILD.should be_an(Integer)
+    Valet::VERSION::BUILD.should be_an(Integer)
   end
 
   it "has a RubyGems compliant version string" do
-    VERSION::STRING.should match(/^\d+\.\d+\.\d+(\.rc\d+)?$/)
+    Valet::VERSION::STRING.should match(/^\d+\.\d+\.\d+(\.rc\d+)?$/)
   end
 end
