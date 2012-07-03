58cc959f6135f435875069fa7426ff794d9e6397
Alexander Baumann <alexander.baumann@arclight.ch>
2012-07-03 11:34:43 +0200

    RSpec examples may now be tagged with :focus to run them exclusively

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,4 +18,8 @@ include Valet
 # Configure RSpec
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
+
+  # Use a :focus tag for running only specific example(s)
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
 end
