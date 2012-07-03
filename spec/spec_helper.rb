d10a9498480f3bf630e44dbab46e4dd67e38814d
Alexander Baumann <alexander.baumann@arclight.ch>
2012-07-03 12:27:18 +0200

    Fixed a problem with RSpec's filter config (Rubinius, JRuby)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,6 +20,6 @@ RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
 
   # Use a :focus tag for running only specific example(s)
-  config.filter_run focus: true
+  config.filter_run :focus
   config.run_all_when_everything_filtered = true
 end
