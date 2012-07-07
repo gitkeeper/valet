0c7b25aa91a10438d4e8f3968fec3f9403a8138a
Alexander Baumann <alexander.baumann@arclight.ch>
2012-07-07 13:24:02 +0200

    Removed copyright and license attributes as they should appear in a template

--- lib/valet/application.rb
+++ lib/valet/application.rb
@@ -1,11 +1,9 @@
 module Valet
   module Application
-    attr_accessor :name, :version, :copyright, :license
+    attr_accessor :name, :version
 
     def start
       puts "#{name} #{version}"
-      puts "#{copyright}"
-      puts "#{license}"
     end
   end
 end
