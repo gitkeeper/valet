6e1931b9fc40cf252acd38dadf943f30125b707f
Alexander Baumann <alexander.baumann@arclight.ch>
2012-07-09 08:57:19 +0200

    New spec helper helps mocking IO streams and resets arguments

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,14 +15,6 @@ Dir[File.expand_path('spec/support/**/*.rb')].each { |file| require file }
 require 'valet'
 include Valet
 
-# Capture STDOUT and STDIN in new IO streams
-require 'stringio'
-$stdout = StringIO.new
-$stderr = StringIO.new
-
-# Reset ARGV to contain no arguments
-ARGV.clear
-
 # Configure RSpec
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
