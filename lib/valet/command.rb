41165f9550bc0a02e74d1ad74bd4997bbccb7800
Alexander Baumann <alexander.baumann@arclight.ch>
2012-07-11 21:05:08 +0200

    Added core attributes and methods, removed DSL method for the moment

--- lib/valet/command.rb
+++ lib/valet/command.rb
@@ -1,29 +1,61 @@
 module Valet
   class Command
-    attr_reader :name, :options
+    attr_reader :name, :aliases, :options, :commands, :examples
     attr_accessor :syntax, :summary, :description
 
     def initialize(name, &block)
+      validate_name(name)
       @name     = name
+      @aliases  = []
       @action   = block
       @options  = Options.new
+      @commands = Commands.new
+      @examples = []
     end
 
     def action(&block)
-      @action = block if block_given?
+      if block_given?
+        validate_action(block)
+        @action = block
+      end
+
       @action
     end
 
-    def option(name, params = {})
-      if params[:type]
-        options << Option::Flag.new(name, params)
+    def execute(*operands)
+      if action.arity == 0
+        action.call
+      elsif action.arity == 1
+        action.call(options)
       else
-        options << Option::Switch.new(name, params)
+        action.call(operands, options)
       end
     end
 
-    def execute(*operands)
-      action.arity == 0 ? action.call : action.call(options, *operands)
+    private
+
+    def validate_name(name)
+      unless valid_name?(name)
+        raise CommandError,
+          "Name: '#{name}' must be a symbol"
+      end
+    end
+
+    def valid_name?(name)
+      name.is_a?(Symbol)
+    end
+
+    def validate_action(proc)
+      unless valid_action?(proc)
+        raise CommandError,
+          "Action: '#{action}' may not have more than two block parameters"
+      end
+    end
+
+    def valid_action?(proc)
+      proc.arity <= 2 && proc.arity >= 0
     end
   end
+
+  class CommandError < StandardError; end
 end
